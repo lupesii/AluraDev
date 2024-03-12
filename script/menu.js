2770a4fca31c34797ca071d29270fee1f5271787
Lucas Silva e Pereira <lucaspereira.dev@proton.me>
2024-03-12 20:46:25 -0300

    Página de edição de código para celulares

---
+++ script/menu.js
@@ -0,0 +1,19 @@
+const openSearch = document.getElementById('openSearch');
+const closeSearch = document.getElementById('closeSearch');
+const searchBar = document.getElementById('searchBar');
+
+const buttonMenu = document.getElementById('buMenu');
+const menu = document.getElementById('menu');
+
+
+openSearch.onclick = function(){
+    searchBar.classList.add("is-visible");
+}
+
+closeSearch.onclick = function(){
+    searchBar.classList.remove("is-visible");
+}
+
+buttonMenu.onclick = function(){
+    menu.classList.toggle("is-open");
+}
